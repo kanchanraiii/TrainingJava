b3870bfb210ecfd9847dc5918f5cdf1e1c44a200
Kanchan Rai <kanchanrai2307@gmail.com>
2025-11-15 14:57:17 +0530

    updated code

--- 15 Nov/OneToMany-Demo/src/main/java/com/OneToMany/requests/Customer.java
+++ 15 Nov/OneToMany-Demo/src/main/java/com/OneToMany/requests/Customer.java
@@ -1,5 +1,46 @@
 package com.OneToMany.requests;
 
+import jakarta.persistence.*;
+import java.util.List;
+
+@Entity
 public class Customer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
+    private List<Order1> orders;
+
+    public Long getId() { 
+        return id; 
+    }
+
+    public void setId(Long id) { 
+        this.id = id; 
+    }
+
+    public String getName() { 
+        return name; 
+    }
+
+    public void setName(String name) { 
+        this.name = name; 
+    }
+
+    public List<Order1> getOrders() { 
+        return orders; 
+    }
+
+    public void setOrders(List<Order1> orders) {
+        this.orders = orders;
+        if (orders != null) {
+            for (Order1 o : orders) {
+                o.setCustomer(this);   // set FK
+            }
+        }
+    }
 }
