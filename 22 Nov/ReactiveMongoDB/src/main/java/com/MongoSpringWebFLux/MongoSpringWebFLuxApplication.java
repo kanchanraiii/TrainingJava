4c7b1c3eeaa2dc6b8632a31891f7f10a83c8fb83
Kanchan Rai <kanchanrai2307@gmail.com>
2025-11-22 12:39:43 +0530

    added: reactive mongodb example

---
+++ 22 Nov/ReactiveMongoDB/src/main/java/com/MongoSpringWebFLux/MongoSpringWebFLuxApplication.java
@@ -0,0 +1,13 @@
+package com.MongoSpringWebFLux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoSpringWebFLuxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoSpringWebFLuxApplication.class, args);
+	}
+
+}
